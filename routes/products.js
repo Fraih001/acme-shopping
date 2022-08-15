1c5bf5f745616486eb80909e7a80b49a78f93fee
Fraih001 <90857197+Fraih001@users.noreply.github.com>
2022-08-15 19:12:40 -0400

    add api support(igdb) including functionality to fetch data from external api then populate Product model w/ said data

--- routes/products.js
+++ routes/products.js
@@ -1,21 +1,20 @@
-// const express = require('express');
-// const app = express.Router();
-// const { Product } = require('../db');
-// const { isLoggedIn } = require('./middleware');
+const express = require('express');
+const app = express.Router();
+const { Product } = require('../db');
+const { isLoggedIn } = require('./middleware');
 
-// app.get('/api/products/', isLoggedIn, async(req,res,next)=>{
-//     try{
-//         if(!user){
-//       res.status(200).send(await Product.findAll())
-//         }
+app.get('/products', isLoggedIn, async(req,res,next)=>{
+    try{
+      res.status(200).send(await Product.findAll())
+        
   
-//     }catch(er){
-//       next(er);
-//     }
-//   });
+    }catch(er){
+      next(er);
+    }
+  });
 
-// // app.get('/', isLoggedIn, async(req, res, next)=> {
-// //   res.send(req.user);
-// // });
+app.get('/', isLoggedIn, async(req, res, next)=> {
+  res.send(req.user);
+});
 
-// module.exports = app;
+module.exports = app;
